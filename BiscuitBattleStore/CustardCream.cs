ac8818ece1fb8b4e59e38aa12167384c6ba20f41
Benjamin Smith <36671994+BenjaminSmith1@users.noreply.github.com>
2019-06-18 13:46:03 +0100

    Working Task 1

---
+++ BiscuitBattleStore/CustardCream.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BiscuitBattle.Store.Decks;
+
+namespace BiscuitBattle.Store
+{
+    //chose custard creams because i was eating one when i started this
+    //code was adapted from the NiceBiscuit class
+    public class CustardCream : IBiscuitCard
+    {
+        public string Name { get; set; }
+        public Dictionary<BiscuitAttribute, int> Stats { get; set; }
+        public int Quality => Stats.Sum(att => att.Value);
+        public string Description { get; set; }
+
+        public CustardCream()
+        {
+
+            //some random numbers
+            Stats = new Dictionary<BiscuitAttribute, int>
+            {
+                { BiscuitAttribute.DunkIntegrity, 70 },
+                { BiscuitAttribute.Moistness, 50 },
+                { BiscuitAttribute.Snap, 30 },
+                { BiscuitAttribute.Sweetness, 60 },
+                { BiscuitAttribute.Texture, 80 }
+            };
+
+            Name = "Custard Cream";
+            //how do you even describe a custard cream???????
+            Description = "A sandwich biscuit with a creamy, custard-flavoured centre surrounded by ornately decorated biscuit layers ";
+        }
+    }
+}
---
+++ BiscuitBattleStore/RandomBiscuitCardFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BiscuitBattle.Store.Decks;
+
+namespace BiscuitBattle.Store
+{
+    public class RandomBiscuitCardFactory : BaseBiscuitFactory
+    {
+        public override IBiscuitCard GetCard()
+        {
+            var ran = new Random();
+            var choose = ran.Next(1, 7);
+            switch (choose)
+            {
+                case 1:
+                    return new NiceBiscuit();
+                case 2:
+                    return new DigestiveBiscuit();
+                case 3:
+                    return new PinkWaferBiscuit();
+                case 4:
+                    return new BalancedBiscuit();
+                case 5:
+                    return new GingernutBiscuit();
+                case 6:
+                    return new CustardCream();
+                default:
+                    return new BalancedBiscuit();
+            }
+        }
+    }
+}
